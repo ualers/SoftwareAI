eef56f0b36e7cea9cb6df793aee40f2d47b66b5a
ualers <125712906+ualers@users.noreply.github.com>
2024-03-23 15:25:24 -0300

    Update keys_def.py

--- keys_def.py
+++ keys_def.py
@@ -1,14 +1,14 @@
 class chaves:
     def chaves_open_ai():
-        api_key = "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"
+        api_key = "key"
         return api_key
     
     def keys_bot_telegram():
-        TOKEN = "6727382277:AAGIDqqusC_JcxYqPhG13vdIlGp-5Kjk83M"
-        CHANNEL_ID = '-1001921761106'
+        TOKEN = "key"
+        CHANNEL_ID = 'key'
         return TOKEN, CHANNEL_ID
     
     def asst_e_threead():
-        asst = 'asst_WS6iiVew2K7ehtvs6eOhbROu'
-        threead_id = 'thread_AG24wZGD1MlsZcgwQXBPK1cc'
+        asst = 'key'
+        threead_id = 'key'
         return asst, threead_id
