951bbf7a46b8ce01ad735daa395b3608fc883bff
ualers <125712906+ualers@users.noreply.github.com>
2024-03-23 15:24:45 -0300

    Add files via upload

---
+++ keys_def.py
@@ -0,0 +1,14 @@
+class chaves:
+    def chaves_open_ai():
+        api_key = "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"
+        return api_key
+    
+    def keys_bot_telegram():
+        TOKEN = "6727382277:AAGIDqqusC_JcxYqPhG13vdIlGp-5Kjk83M"
+        CHANNEL_ID = '-1001921761106'
+        return TOKEN, CHANNEL_ID
+    
+    def asst_e_threead():
+        asst = 'asst_WS6iiVew2K7ehtvs6eOhbROu'
+        threead_id = 'thread_AG24wZGD1MlsZcgwQXBPK1cc'
+        return asst, threead_id
