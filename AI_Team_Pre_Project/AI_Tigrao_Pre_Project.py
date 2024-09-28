6aef336cc5367f36bbe1db6a6c3bf96d42e74576
ualers <125712906+ualers@users.noreply.github.com>
2024-09-28 09:21:55 -0300

    Add files via upload

---
+++ AI_Team_Pre_Project/AI_Tigrao_Pre_Project.py
@@ -0,0 +1,92 @@
+
+import subprocess
+import threading
+from CoreApp.SoftwareAI_Core import AutenticateAgent, ResponseAgent, python_functions
+from CoreApp.Cliente_cli import ChatCLI
+import sys
+import json
+# SoftwareAI
+##############################################################################################
+class Pre_Project_Document:
+
+
+    def AI_1_Pre_Project_Document_Writer(mensagem):
+        """
+        Nome da IA: Tigrao \n
+        Função: Escritor de Documento Pre-Projeto De Software \n
+        Horario de trabalho: 1H\n
+        :param mensagem: str \n
+        :param output: path_name_doc_Pre_Projeto
+        """   
+
+       
+
+
+        instructionAI_Escritor_de_documento_Pre_Projeto = """ 
+        Seu Nome é Tigrao, Voce é um Escritor de Documento Pre-Projeto De Software da empresa Urobotsoftware,
+        crie o documento com as informacoes fornecidas para que o documento criado por voce posteriormente seja enviados para o Gerente de Projeto que analizará o documento e criará o Cronograma e planilha do Projeto
+
+        Responda o Documento Pre-Projeto no formato JSON
+        Exemplo:
+        {"doc_Pre_Projeto": "documento Pre-Projeto ..."}
+
+        Responda o nome do Documento Pre-Projeto no formato JSON
+        Exemplo:
+        {"name_doc_Pre_Projeto": "nome do Documento ..."}
+
+        """
+        key = "AI_Tigrao_Escritor_de_documento_Pre_Projeto"
+        nameassistant = "AI Tigrao Escritor de documento Pre-Projeto"
+        model_select = "gpt-4o-mini-2024-07-18"
+        adxitional_instructions = None
+        Upload_1_file_in_thread = None
+        Upload_1_file_in_message = None
+        Upload_1_image_for_vision_in_thread = None
+        Upload_list_for_code_interpreter_in_thread = None
+        vectorstore_in_Thread = None
+        vectorstore_in_agent = None
+        tools = None
+        
+           
+
+        AI_Escritor_de_documento_Pre_Projeto = AutenticateAgent.create_or_auth_AI(key, instructionAI_Escritor_de_documento_Pre_Projeto, nameassistant, model_select, tools, vectorstore_in_agent)
+
+        mensaxgem = f"crie um documento Pre-Projeto baseado nas informacoes asseguir: \n  {mensagem}"
+
+        response = ResponseAgent.ResponseAgent_message_with_assistants(mensaxgem,
+                                                                    Upload_1_file_in_thread, Upload_1_file_in_message,
+                                                                    Upload_1_image_for_vision_in_thread, Upload_list_for_code_interpreter_in_thread, vectorstore_in_Thread,
+                                                                    tools,  AI_Escritor_de_documento_Pre_Projeto, model_select, adxitional_instructions, key)
+        #print(response)
+        
+          
+
+        path_name_doc_Pre_Projeto = f"Work_Environment/Create_doc_Pre_Projeto/documento_pre_projeto.txt"
+        python_functions.save_TXT(response, path_name_doc_Pre_Projeto, "w")
+        #python_functions.save_json(response, path_name_doc_Pre_Projeto)
+        try:
+            teste_dict = json.loads(response)
+            documento_Pre_Projeto = teste_dict['doc_Pre_Projeto']
+            print(documento_Pre_Projeto)
+            
+            name_doc_Pre_Projeto = teste_dict['name_doc_Pre_Projeto']
+            path_name_doc_Pre_Projeto = f"Work_Environment/Create_doc_Pre_Projeto/{name_doc_Pre_Projeto}.txt"
+            print(name_doc_Pre_Projeto)
+            print(path_name_doc_Pre_Projeto)
+
+            python_functions.save_TXT(documento_Pre_Projeto, path_name_doc_Pre_Projeto, "w")
+            return path_name_doc_Pre_Projeto
+        except:
+            
+
+            
+            return path_name_doc_Pre_Projeto
+
+
+
+
+
+
+
+
+
