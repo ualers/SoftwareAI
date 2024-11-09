173d6653dd0551de9bab9cf31e97940c736321b5
ualers <125712906+ualers@users.noreply.github.com>
2024-11-09 11:40:10 -0300

    Add _init_core_.py

---
+++ CoreApp/_init_core_.py
@@ -0,0 +1,9 @@
+from .SoftwareAI.SoftwareAI_Core import (
+        AutenticateAgent,
+        ResponseAgent,
+        Github_functions,
+        python_functions,
+        Agent_files,
+        Agent_files_update
+)
+
