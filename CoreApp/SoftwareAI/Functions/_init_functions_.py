5eda4db3fb301be25ef4388030a674a1fb73b6d5
ualers <125712906+ualers@users.noreply.github.com>
2024-11-09 11:39:14 -0300

    Add _init_functions_.py

---
+++ CoreApp/SoftwareAI/Functions/_init_functions_.py
@@ -0,0 +1,6 @@
+from .get_datetime_function import *
+from .run_testes_function import *
+from .create_github_function import *
+from .improve_code_function import *
+from .review_pr_function import *
+from .add_project_map import *
