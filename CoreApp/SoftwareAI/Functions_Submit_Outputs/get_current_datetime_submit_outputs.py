a5f55af5622af58166de4140f571e75dce4b5404
ualers <125712906+ualers@users.noreply.github.com>
2024-11-16 13:13:54 -0300

    Add get_current_datetime_submit_outputs.py

--- CoreApp/SoftwareAI/Functions_Submit_Outputs/get_current_datetime_submit_outputs.py
+++ CoreApp/SoftwareAI/Functions_Submit_Outputs/get_current_datetime_submit_outputs.py
@@ -6,7 +6,7 @@ from CoreApp._init_libs_ import *
 # IMPORT SoftwareAI Functions
 from ..Functions._init_functions_ import *
 #########################################
-
+tool_outputs = []
 def submit_output_get_current_datetime(function_name,
                                 function_arguments,
                                 tool_call,
@@ -15,19 +15,28 @@ def submit_output_get_current_datetime(function_name,
                                 run
                                 ):
 
-
+    global tool_outputs
     if function_name == 'get_current_datetime':
-        args = json.loads(function_arguments)
-        result = get_current_datetime()
-        tool_call_id = tool_call.id
-        client.beta.threads.runs.submit_tool_outputs(
+    
+        resultado = get_current_datetime()
+        tool_outputs.append({
+        "tool_call_id": tool_call.id,
+        "output": resultado
+        })
+
+    
+    # Submit all tool outputs at once after collecting them in a list
+    if tool_outputs:
+        try:
+            run = client.beta.threads.runs.submit_tool_outputs_and_poll(
             thread_id=threead_id,
             run_id=run.id,
-            tool_outputs=[
-                {
-                    "tool_call_id": tool_call_id,
-                    "output": json.dumps(result)
-                }
-            ]
-        )
-
+            tool_outputs=tool_outputs
+            )
+            print("Tool outputs submitted successfully.")
+            tool_outputs = []
+            return True
+        except Exception as e:
+            print("Failed to submit tool outputs:", e)
+    else:
+        print("No tool outputs to submit.")
