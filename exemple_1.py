a127afd3254f7165cd171c9beb5b1df51549c7b6
ualers <125712906+ualers@users.noreply.github.com>
2024-11-16 13:28:16 -0300

    Add exemple_1.py

--- exemple_1.py
+++ exemple_1.py
@@ -9,7 +9,6 @@ from CoreApp._init_libs_ import *
 
 byte_manager = AgentInitializer.get_agent('ByteManager') 
 
-mensagem = "solicito um script para Criar pdf com base em tutoriais do youtube o objetivo é extrair audio do video e transcrever utilizando whisper local modelo small e criar o pdf com base nos audios dos tutoriais  "
+mensagem = "solicito um script para Análise técnica da criptomoeda solana  "
 owner_response = byte_manager.AI_1_ByteManager_Company_Owners(mensagem)
 print(owner_response)
-
