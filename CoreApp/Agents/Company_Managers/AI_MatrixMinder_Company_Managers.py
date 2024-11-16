21c002afd17bd0af07b43c61a28c7239b2d443c5
ualers <125712906+ualers@users.noreply.github.com>
2024-11-16 13:15:07 -0300

    Add AI_MatrixMinder_Company_Managers.py

--- CoreApp/Agents/Company_Managers/AI_MatrixMinder_Company_Managers.py
+++ CoreApp/Agents/Company_Managers/AI_MatrixMinder_Company_Managers.py
@@ -9,6 +9,12 @@ from ..._init_agents_ import *
 # IMPORT SoftwareAI Libs 
 from ..._init_libs_ import *
 #########################################
+# IMPORT SoftwareAI Instructions
+from ...SoftwareAI.Instructions._init_Instructions_ import *
+#########################################
+# IMPORT SoftwareAI Tools
+from ...SoftwareAI.Tools._init_tools_ import *
+#########################################
 
 
 
@@ -27,21 +33,10 @@ class Company_Managers:
         """Nome da IA: MatrixMinder\n
             Função: Manager de Software"""    
 
-
-        instruction_MatrixMinder = """ 
-     
-        Seu nome é MatrixMinder, você é o Manager de Software da empresa Urobotsoftware. Sua principal responsabilidade é receber e analisar os arquivos enviados por Byte Manager, o CEO da Urobotsoftware, que representam solicitações de atualização de software.
-
-        Seu objetivo é verificar se o que foi solicitado é uma atualização . Caso seja, você deve aprovar a requisição de atualização
-
-        Formato de Resposta:
-        Responda no formato JSON.
-
-        """
         key = "AI_MatrixMinder_Manager_de_Software"
         nameassistant = "AI MatrixMinder Manager de Software"
         model_select = "gpt-4o-mini"
-        adxitional_instructions = None
+        
         Upload_1_file_in_thread = None
         Upload_1_file_in_message = None
         Upload_1_image_for_vision_in_thread = None
@@ -55,9 +50,7 @@ class Company_Managers:
         #     Documentation_md,
         #     Project_spreadsheet_txt
         # ]
-        tools = [
-                 {"type": "file_search"}
-                ]
+
         MatrixMinderAnálxise = "MatrixMinderAnálise"
         file_path_MatrixMinderAnálise = [
             Analysis_txt,
@@ -70,7 +63,7 @@ class Company_Managers:
         vector_store_id_MatrixMinderAnálise = Agent_files.auth_or_create_vectorstore(MatrixMinderAnálxise, file_path_MatrixMinderAnálise)
         vectorstore_in_Thread = vector_store_id_MatrixMinderAnálise
 
-        AI_MatrixMinder = AutenticateAgent.create_or_auth_AI(key, instruction_MatrixMinder, nameassistant, model_select, tools, vectorstore_in_assistant)
+        AI_MatrixMinder = AutenticateAgent.create_or_auth_AI(key, instructionMatrixMinder, nameassistant, model_select, tools_MatrixMinder, vectorstore_in_assistant)
 
         mensaxgem = f"""
         decidir se oque foi solicitado na mensagem é uma atualizacao: {mensaagem}
@@ -81,7 +74,7 @@ class Company_Managers:
                                                                 Upload_1_file_in_thread, Upload_1_file_in_message,
                                                                 Upload_1_image_for_vision_in_thread, Upload_list_for_code_interpreter_in_thread,
                                                                 vectorstore_in_Thread,
-                                                                tools,  AI_MatrixMinder, model_select, adxitional_instructions, key)
+                                                                tools_MatrixMinder,  AI_MatrixMinder, model_select, adxitional_instructions_MatrixMinder, key)
         try:
             resposta = AI_ByteManager_response['resposta']
         except:
