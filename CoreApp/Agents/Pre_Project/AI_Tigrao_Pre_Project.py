8af9551eda19314b90749e941998e5f189c15b1c
ualers <125712906+ualers@users.noreply.github.com>
2024-11-16 13:15:08 -0300

    Add AI_Tigrao_Pre_Project.py

--- CoreApp/Agents/Pre_Project/AI_Tigrao_Pre_Project.py
+++ CoreApp/Agents/Pre_Project/AI_Tigrao_Pre_Project.py
@@ -9,6 +9,12 @@ from ..._init_libs_ import *
 # IMPORT SoftwareAI All Paths 
 from ..._init_paths_ import *
 #########################################
+# IMPORT SoftwareAI Instructions
+from ...SoftwareAI.Instructions._init_Instructions_ import *
+#########################################
+# IMPORT SoftwareAI Tools
+from ...SoftwareAI.Tools._init_tools_ import *
+#########################################
 
 
 class Pre_Project_Document:
@@ -27,48 +33,30 @@ class Pre_Project_Document:
 
        
 
-
-        instructionAI_Escritor_de_documento_Pre_Projeto = """ 
-        Seu Nome é Tigrao, Voce é um Escritor de Documento Pre-Projeto De Software da empresa Urobotsoftware,
-        crie o documento com as informacoes fornecidas para que o documento criado por voce posteriormente seja enviados para o Gerente de Projeto que analizará o documento e criará o Cronograma e planilha do Projeto
-
-        Responda o Documento Pre-Projeto no formato JSON
-        Exemplo:
-        {"doc_Pre_Projeto": "documento Pre-Projeto ..."}
-
-        Responda o nome do Documento Pre-Projeto no formato JSON
-        Exemplo:
-        {"name_doc_Pre_Projeto": "nome do Documento ..."}
-
-        """
         key = "AI_Tigrao_Escritor_de_documento_Pre_Projeto"
         nameassistant = "AI Tigrao Escritor de documento Pre-Projeto"
         model_select = "gpt-4o-mini-2024-07-18"
-        adxitional_instructions = None
+        
         Upload_1_file_in_thread = None
         Upload_1_file_in_message = None
         Upload_1_image_for_vision_in_thread = None
         Upload_list_for_code_interpreter_in_thread = None
         vectorstore_in_Thread = None
         vectorstore_in_agent = None
-        tools = None
-        
+
            
 
-        AI_Escritor_de_documento_Pre_Projeto = AutenticateAgent.create_or_auth_AI(key, instructionAI_Escritor_de_documento_Pre_Projeto, nameassistant, model_select, tools, vectorstore_in_agent)
+        AI_Escritor_de_documento_Pre_Projeto = AutenticateAgent.create_or_auth_AI(key, instructionTigrao, nameassistant, model_select, tools_Tigrao, vectorstore_in_agent)
 
         mensaxgem = f"crie um documento Pre-Projeto baseado nas informacoes asseguir: \n  {mensagem}"
-
-        response = ResponseAgent.ResponseAgent_message_with_assistants(mensaxgem,
+        format = 'Responda no formato JSON Exemplo: {"doc_Pre_Projeto": "..."},{"name_doc_Pre_Projeto": "..."} '
+        mensage3 = mensaxgem + format
+        response = ResponseAgent.ResponseAgent_message_with_assistants(mensage3,
                                                                     Upload_1_file_in_thread, Upload_1_file_in_message,
                                                                     Upload_1_image_for_vision_in_thread, Upload_list_for_code_interpreter_in_thread, vectorstore_in_Thread,
-                                                                    tools,  AI_Escritor_de_documento_Pre_Projeto, model_select, adxitional_instructions, key)
+                                                                    tools_Tigrao,  AI_Escritor_de_documento_Pre_Projeto, model_select, adxitional_instructions_Tigrao, key)
         #print(response)
         
-          
-
-        path_name_doc_Pre_Projeto = os.getenv("PATH_NAME_DOC_PRE_PROJETO_ENV")
-        python_functions.save_TXT(response, path_name_doc_Pre_Projeto, "w")
         #python_functions.save_json(response, path_name_doc_Pre_Projeto)
         try:
             teste_dict = json.loads(response)
@@ -76,17 +64,19 @@ class Pre_Project_Document:
             print(documento_Pre_Projeto)
             
             name_doc_Pre_Projeto = teste_dict['name_doc_Pre_Projeto']
-            path_name_doc_Pre_Projeto = f"Work_Environment/Create_doc_Pre_Projeto/{name_doc_Pre_Projeto}.txt"
+            path_name_doc_Pre_Projeto = os.getenv("PATH_NAME_DOC_PRE_PROJETO_ENV")
+            path = os.path.dirname(path_name_doc_Pre_Projeto)
+            novo_caminho = os.path.join(path, name_doc_Pre_Projeto)
             print(name_doc_Pre_Projeto)
-            print(path_name_doc_Pre_Projeto)
+           
 
-            python_functions.save_TXT(documento_Pre_Projeto, path_name_doc_Pre_Projeto, "w")
+            python_functions.save_TXT(documento_Pre_Projeto, novo_caminho, "w")
             return path_name_doc_Pre_Projeto
-        except:
-            pass
-
-            
-        return path_name_doc_Pre_Projeto
+        except Exception as E:
+            print(E)
+            path_name_doc_Pre_Projeto = os.getenv("PATH_NAME_DOC_PRE_PROJETO_ENV")
+            python_functions.save_TXT(response, path_name_doc_Pre_Projeto, "w")
+            return path_name_doc_Pre_Projeto
 
 
 
