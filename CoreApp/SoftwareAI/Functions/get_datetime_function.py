21669e988ad3eeaff52ccd06c033080e46008138
ualers <125712906+ualers@users.noreply.github.com>
2024-11-16 13:13:30 -0300

    Add get_datetime_function.py

--- CoreApp/SoftwareAI/Functions/get_datetime_function.py
+++ CoreApp/SoftwareAI/Functions/get_datetime_function.py
@@ -3,8 +3,4 @@ def get_current_datetime():
     import datetime
     current_datetime = datetime.datetime.now()
     formatted_datetime = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
-    return formatted_datetime
-
-def handle_get_current_datetime():
-    report = get_current_datetime()
-    return {"report": report}
+    return f"{formatted_datetime}"
