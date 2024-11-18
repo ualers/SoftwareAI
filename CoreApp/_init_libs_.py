bf7ee32c0e4723f37dbe8b5312a261ab61a746a8
ualers <125712906+ualers@users.noreply.github.com>
2024-11-17 23:46:25 -0300

    Add _init_libs_.py

--- CoreApp/_init_libs_.py
+++ CoreApp/_init_libs_.py
@@ -11,11 +11,8 @@ import json
 from firebase_admin import credentials, initialize_app, storage, db, delete_app
 from openai import OpenAI
 import time
-import subprocess
 import pandas as pd
-import os
 import shutil
-import json
 import tiktoken
 from github import Github
 import re
@@ -23,17 +20,8 @@ import requests
 import base64
 import random
 from datetime import datetime, timedelta
-import json
 import struct
 from dotenv import load_dotenv, find_dotenv
-import os
-import os
 import git
-import os
-import requests
 from requests.auth import HTTPBasicAuth
-import base64
-import os
-import base64
-import requests
 #########################################
