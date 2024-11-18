d11ab716c250f8d10f56b67cd4149291dc78d43a
ualers <125712906+ualers@users.noreply.github.com>
2024-11-17 23:46:27 -0300

    Add _init_paths_.py

--- CoreApp/_init_paths_.py
+++ CoreApp/_init_paths_.py
@@ -2,6 +2,6 @@
 from ._init_libs_ import *
 #########################################
 # IMPORT SoftwareAI All Paths 
-load_dotenv(dotenv_path=r"C:\Users\Media Cuts Studio\Desktop\Saas do site\Projetos de codigo aberto\SoftwareAI\CoreApp\ambiente.env")
-#########################################
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), "ambiente.env"))
 load_dotenv(find_dotenv('ambiente.env'), override=True)
+#########################################
