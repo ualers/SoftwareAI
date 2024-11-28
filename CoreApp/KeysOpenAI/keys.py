423f11ca56d31588a149c88bab1379954975639f
ualers <125712906+ualers@users.noreply.github.com>
2024-11-28 10:15:35 -0300

    Add keys.py

---
+++ CoreApp/KeysOpenAI/keys.py
@@ -0,0 +1,11 @@
+#########################################
+# IMPORT SoftwareAI Libs 
+from CoreApp._init_libs_ import *
+#########################################
+
+class OpenAIKeys:
+    def keys_openai():
+        "Key Open ai"
+        str_key = "youkey"
+        return str_key
+    
